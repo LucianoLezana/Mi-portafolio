1af23a0b74ca042f0374d37393e6e13ed9faeb2c
Luciano Lezana <luchobenjo50@gmail.com>
2022-10-13 20:48:31 -0300

    Retoques al form contactme

--- WebLucianoApp/models.py
+++ WebLucianoApp/models.py
@@ -1,9 +1,13 @@
+from audioop import reverse
 import email
 from unicodedata import name
 from django.db import models
+from django.urls import reverse
 
 class ContactMe(models.Model):
     name = models.CharField(max_length=150)
     email = models.EmailField()
     subject =models.CharField(max_length=150)
     menssage = models.CharField(max_length=150)
+    def get_absolute_url(self):
+        return reverse('home')
